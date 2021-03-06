cf70d77ef29cf0cbdc49496337f2bcd3681d90a7
dlswer23 <lbj4298@naver.com>
2021-03-06 21:50:15 +0900

    [ADD] 아스키 코드

---
+++ Java/아스키코드.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class 아스키코드 {
+
+    public static void main(String[] args) throws IOException {
+
+
+        //read 함수를 사용
+        int c = System.in.read();
+        System.out.println(c);
+
+    }
+}
